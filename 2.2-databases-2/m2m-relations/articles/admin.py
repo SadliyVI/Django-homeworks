bc4498ef09ede4dd4ecb9d7d781980b35b3f23ae
V.Sadliy <sadliy@mail.ru>
2025-05-21 10:36:14 +0300

    Added HW 2.2-databases-2  m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,36 +1,49 @@
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
-
 from .models import Article, Tag, Scope
 
 
-class ScopeInlineFormset(BaseInlineFormSet):
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
         main_count = 0
+        tags_set = set()
         for form in self.forms:
+            if self._should_delete_form(form):
+                continue
+            if not form.cleaned_data:
+                continue
+            tag_name = form.cleaned_data.get('tag')
+            article = form.cleaned_data.get('article')
+            if not self._should_delete_form(form):
+                if not tag_name:
+                    raise ValidationError('Тег является обязательным полем!')
+                if not article:
+                    raise ValidationError(
+                        'Статья является обязательным полем!')
             if form.cleaned_data.get('is_main'):
                 main_count += 1
-        if main_count == 0:
-            raise ValidationError('Необходимо выбрать основной раздел!')
-        elif main_count > 1:
-            raise ValidationError('Основным может быть только один раздел')
+            if tag_name in tags_set:
+                raise ValidationError(f'Тег "{tag_name}" уже существует!')
+            tags_set.add(tag_name)
+        if main_count > 1:
+            raise ValidationError('Основной тэг может быть только один!')
+        elif main_count == 0:
+            raise ValidationError(
+                'Необходимо установить хоть один тэг как основной!')
 
         return super().clean()
-
-
 class RelationshipInline(admin.TabularInline):
     model = Scope
-    formset = ScopeInlineFormset
+    formset = RelationshipInlineFormset
     extra = 1
 
-
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ObjectAdmin(admin.ModelAdmin):
     inlines = [RelationshipInline]
-
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
-
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+
+
 class Tag(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название раздела')
+    name = models.CharField(max_length=30, verbose_name='Название тэга')
 
     class Meta:
-        verbose_name = 'Раздел'
-        verbose_name_plural = 'Разделы'
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
-
 
 class Article(models.Model):
 
@@ -17,6 +19,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, related_name='article', through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -26,11 +29,9 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
 class Scope(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел')
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья')
-    is_main = models.BooleanField(verbose_name='Основной')
 
-    def __str__(self):
-        return self.tag
-
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='основной тэг')
